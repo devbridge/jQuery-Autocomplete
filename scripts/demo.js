8211e43cfdb81f97f851ef3b27e4c19825bff37a
Tomas Kirda <tomas@devbridge.com>
2012-11-10 21:44:42 -0600

    Add working demo.

---
+++ scripts/demo.js
@@ -0,0 +1,23 @@
+﻿/*jslint  browser: true, white: true, plusplus: true */
+/*global $: true */
+
+$(function () {
+    'use strict';
+
+    $.ajax({
+        url: 'content/countries.txt',
+        dataType: 'json'
+    }).done(function (data) {
+        var status = $('#selection'),
+            countries = $.map(data, function (value) {
+                return value;
+            });
+
+        $('#query').autocomplete({
+            lookup: countries,
+            onSelect: function (suggestion) {
+                status.html('You selected: ' + suggestion);
+            }
+        });
+    });
+});
