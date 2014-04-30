cbdd226c0d18cda656379d360ba2407f5219ec3e
acroyear2 <qophee@gmail.com>
2014-04-30 15:39:22 +0300

    support browserify

--- src/jquery.autocomplete.js
+++ src/jquery.autocomplete.js
@@ -16,6 +16,9 @@
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
         define(['jquery'], factory);
+    } else if (typeof exports === 'object') {
+        // browserify
+        factory(require('jquery'));
     } else {
         // Browser globals
         factory(jQuery);
