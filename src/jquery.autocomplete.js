8e6812d6036515ba46d5d581fc7a1d59ae4e5815
Tomas Kirda <tomas@devbridge.com>
2015-03-04 10:46:03 -0600

    Fix white space.

--- src/jquery.autocomplete.js
+++ src/jquery.autocomplete.js
@@ -1,6 +1,6 @@
 /**
 *  Ajax Autocomplete for jQuery, version %version%
-*  (c) 2014 Tomas Kirda
+*  (c) 2015 Tomas Kirda
 *
 *  Ajax Autocomplete for jQuery is freely distributable under the terms of an MIT-style license.
 *  For details, see the web site: https://github.com/devbridge/jQuery-Autocomplete
@@ -615,11 +615,10 @@
             var that = this,
             container = $(that.suggestionsContainer);
 
-            if ($.isFunction(this.options.onHide) && that.visible) {
-
-              this.options.onHide.call(that.element, container);
+            if ($.isFunction(that.options.onHide) && that.visible) {
+                that.options.onHide.call(that.element, container);
             }
-            
+
             that.visible = false;
             that.selectedIndex = -1;
             clearInterval(that.onChangeInterval);
@@ -629,12 +628,11 @@
 
         suggest: function () {
             if (this.suggestions.length === 0) {
-                this.options.showNoSuggestionNotice ? this.noSuggestions() : this.hide();
-				if (this.options.showNoSuggestionNotice) {
-					this.noSuggestions();
-				} else {
-					this.hide();
-				}
+                if (this.options.showNoSuggestionNotice) {
+                    this.noSuggestions();
+                } else {
+                    this.hide();
+                }
                 return;
             }
 
