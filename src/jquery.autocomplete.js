3328147b93c1650beb78278b41aab79ad4ad9173
Tomas Kirda <tomas@devbridge.com>
2015-07-29 15:51:36 +0300

    Fix issue of highlighting string when it contains html specific characters, fixes #385

--- src/jquery.autocomplete.js
+++ src/jquery.autocomplete.js
@@ -127,15 +127,15 @@
     $.Autocomplete = Autocomplete;
 
     Autocomplete.formatResult = function (suggestion, currentValue) {
-        var htmlSafeString = suggestion.value
+        var pattern = '(' + utils.escapeRegExChars(currentValue) + ')';
+        
+        return suggestion.value
+            .replace(new RegExp(pattern, 'gi'), '<strong>$1<\/strong>')
             .replace(/&/g, '&amp;')
             .replace(/</g, '&lt;')
             .replace(/>/g, '&gt;')
-            .replace(/"/g, '&quot;');
-
-        var pattern = '(' + utils.escapeRegExChars(currentValue) + ')';
-
-        return htmlSafeString.replace(new RegExp(pattern, 'gi'), '<strong>$1<\/strong>');
+            .replace(/"/g, '&quot;')
+            .replace(/&lt;(\/?strong)&gt;/g, '<$1>');
     };
 
     Autocomplete.prototype = {
