d2cd54a2d879d3d74fc02443f4ccc0cae6bcfc44
Tomas Kirda <tomas@devbridge.com>
2015-09-07 12:45:45 -0500

    Rev for 1.2.23 release

--- dist/jquery.autocomplete.js
+++ dist/jquery.autocomplete.js
@@ -1,5 +1,5 @@
 /**
-*  Ajax Autocomplete for jQuery, version 1.2.22
+*  Ajax Autocomplete for jQuery, version 1.2.23
 *  (c) 2015 Tomas Kirda
 *
 *  Ajax Autocomplete for jQuery is freely distributable under the terms of an MIT-style license.
@@ -346,6 +346,7 @@
             var that = this;
             that.stopKillSuggestions();
             that.intervalId = window.setInterval(function () {
+                that.el.val(that.currentValue);
                 that.hide();
                 that.stopKillSuggestions();
             }, 50);
