74a92c8a687e3a3e8878db8b96fdc755bac7a319
Marcose <xmfdtt123@gmail.com>
2020-10-09 19:04:58 +0530

    New changes again

--- src/jquery.autocomplete.js
+++ src/jquery.autocomplete.js
@@ -13,6 +13,7 @@
 (function (factory) {
     "use strict";
     if (typeof define === 'function' && define.amd) {
+      console.log("RASTERIZING")
         // AMD. Register as an anonymous module.
         define(['jquery'], factory);
     } else if (typeof exports === 'object' && typeof require === 'function') {
